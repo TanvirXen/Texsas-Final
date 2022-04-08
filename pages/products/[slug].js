67214439842c49bde6e00a7f336acde121391ad3
TanvirXen <mail@tanvirishtiaq.com>
2022-04-08 17:38:37 +0600

    New

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -41,7 +41,7 @@ function Products(props) {
 
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<meta property="og:type" content="website" />
-				<meta property="og:url" content={"https://www.texsasbd.com/products"+props.data.slug} />
+				<meta property="og:url" content={"https://www.texsasbd.com/products/"+props.data.slug} />
 				<meta property="og:title" content={"TEXSAS-"+props.data.model} />
 				<meta
 					property="og:description"
